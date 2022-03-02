ccdd726b0be766f12def7c779390c88cde434aef
Pat Welch <pat@mousebrains.com>
2022-03-02 20:05:01 +0000

    Add --dryrun and --remove capabilities

--- syncthing/cleanup.py
+++ syncthing/cleanup.py
@@ -6,9 +6,7 @@
 
 from argparse import ArgumentParser
 import subprocess
-import os
-import socket
-import time
+import shutil
 import sys
 
 def execCmd(cmd:tuple[str], inputText:bytes=None, qIgnoreOutput:bool=False) -> None:
@@ -29,11 +27,12 @@ def myDeviceID(args:ArgumentParser) -> str:
     try:
         return str(deviceID, "utf-8").strip()
     except:
-        print("Unable to convert deviceID to a string", deviceID)
-        sys.exit(1)
+        raise Exception(f"Unable to convert deviceID to a string {deviceID}")
 
-def getList(name:str) -> set:
-    cmd = (args.syncthing, "cli", "config", name, "list")
+def getList(cmd:tuple[str]=None, name:str=None) -> set:
+    if cmd is None:
+        if name is None: raise Exception("You must specify either cmd or name")
+        cmd = (args.syncthing, "cli", "config", name, "list")
     items = set()
     s = subprocess.run(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     if s.returncode:
@@ -48,12 +47,14 @@ def getList(name:str) -> set:
     return items
 
 def getDevices(args:ArgumentParser) -> set:
-    return getList("devices")
+    return getList(name="devices")
 
 def getFolders(args:ArgumentParser) -> set:
-    return getList("folders")
+    return getList(name="folders")
 
 parser = ArgumentParser()
+parser.add_argument("--dryrun", action="store_true", help="Don't actually delete anything")
+parser.add_argument("--remove", action="store_true", help="Remove data folders")
 parser.add_argument("--toKeep", type=str, action="append", help="Folders to not delete")
 grp = parser.add_argument_group(description="Command full paths")
 grp.add_argument("--syncthing", type=str, default="/usr/bin/syncthing",
@@ -67,9 +68,22 @@ myID = myDeviceID(args)
 for device in getDevices(args):
     if device == myID: continue # Ignore my own ID
     print("Deleting", device)
-    execCmd((args.syncthing, "cli", "config", "devices", device, "delete"))
+    if not args.dryrun:
+        execCmd((args.syncthing, "cli", "config", "devices", device, "delete"))
 
 for folder in getFolders(args):
     if folder in toKeep: continue
+    paths = set() # Paths to be deleted, maybe
+    if args.remove: # Going to delete the underlying data
+        if shutil.rmtree.avoids_symlink_attacks:
+            paths = getList(cmd=(args.syncthing, "cli", "config", "folders", folder, "path", "get"))
+        else:
+            print("Not removing since symlink attacks can not be avoided")
+
     print("Deleting", folder)
-    execCmd((args.syncthing, "cli", "config", "folders", folder, "delete"))
+    if not args.dryrun:
+        execCmd((args.syncthing, "cli", "config", "folders", folder, "delete"))
+
+    for path in paths:
+        print("Removing", path)
+        if not args.dryrun: shutil.rmtree(path, ignore_errors=True)
