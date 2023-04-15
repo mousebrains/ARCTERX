b198904bca11953bb0e9dd2c0ce5e8ee4177256c
Pat Welch <pat@mousebrains.com>
2023-04-15 20:20:59 +0000

    Move public_html into git repository space

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>ARCTERX</title>
+</head>
+<body>
+<ul>
+<li><a href='AVISO'>AVISO</a></li>
+<li><a href='links.html'>Links</a></li>
+<li><a href='Sync.ARCTERX/Shore/Papers'>Papers</a></li>
+<li><a href='Sync.ARCTERX/Shore/Software'>Software</a></li>
+<li><a href='Sync.ARCTERX/Ship'>Ship</a></li>
+<li><a href='Sync.ARCTERX/Shore'>Shore</a></li>
+<li><a href='typhoons.html'>Typhoons</a></li>
+</ul>
+</body>
+</html>
