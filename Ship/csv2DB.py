acfa6ca474176b68e871e6f8a2817538eae615a6
Pat Welch <pat@mousebrains.com>
2023-04-27 06:05:35 +0000

    Ship's position from csv to DB

--- Ship/csv2DB.py
+++ Ship/csv2DB.py
@@ -15,8 +15,6 @@ import pyinotify
 import time
 import queue
 import psycopg
-import re
-import glob
 import os.path
 
 class Reader(Thread):
@@ -26,26 +24,24 @@ class Reader(Thread):
 
     def runIt(self) -> None:
         dbOpt = f"dbname={self.args.db}"
-        dt = self.args.delay
-        exp = re.compile(r"\w+[.]\d+[.]csv")
+        args = self.args
+        dt = args.delay
         q = self.__queue
 
-        with psycopg.connect(dbOpt) as db: 
-            for fn in glob.glob(os.path.join(args.csv, "*.csv")):
-                if not exp.fullmatch(os.path.basename(fn)): continue
-                self.__loadFile(db, fn)
+        if os.path.isfile(args.csv):
+            with psycopg.connect(dbOpt) as db: 
+                self.__loadFile(db, args.csv)
 
         while True:
             (t0, fn) = q.get()
             q.task_done()
-            if not exp.fullmatch(os.path.basename(fn)): continue
+            if fn != args.csv: continue
             time.sleep(dt)
             with psycopg.connect(dbOpt) as db: self.__loadFile(db, fn)
 
-    @staticmethod
-    def __loadFile(db, fn:str) -> bool:
-        sql0 = "INSERT INTO ship (id,t,lat,lon,spd,hdg)"
-        sql0+= " VALUES(%s,%s,%s,%s,%s,%s)"
+    def __loadFile(self, db, fn:str) -> bool:
+        sql0 = "INSERT INTO ship (id,t,lat,lon)"
+        sql0+= " VALUES(%s,%s,%s,%s)"
         sql0+= " ON CONFLICT DO NOTHING;"
 
         sql1 = "SELECT position FROM filePosition WHERE filename=%s;"
@@ -53,17 +49,14 @@ class Reader(Thread):
         sql2 = "INSERT INTO filePosition VALUES (%s,%s)"
         sql2+= " ON CONFLICT (filename) DO UPDATE SET position=excluded.position;"
 
-        matches = re.match(r"(\w+)[.]\d+.csv", os.path.basename(fn))
-        if not matches:
-            logging.warning("Bad filename, %s", fn)
-            return False
-        ident = matches[1]
-
         cur = db.cursor()
         pos = None
-        for row in cur.execute(sql1, [fn]):
-            pos = row[0]
-            break
+        if self.args.force:
+            self.args.force = False
+        else:
+            for row in cur.execute(sql1, [fn]):
+                pos = row[0]
+                break
         logging.info("fn %s pos %s", fn, pos)
         cur.execute("BEGIN TRANSACTION;")
         cnt = 0
@@ -72,13 +65,12 @@ class Reader(Thread):
                 fp.seek(pos)
             for line in fp:
                 fields = line.strip().split(",")
-                if len(fields) < 5: continue # Truncated line so ignore
-                if fields[0] == "t": continue
+                if len(fields) < 4: continue # Truncated line so ignore
+                if fields[0] == "id": continue
                 try:
-                    for i in range(len(fields)):
-                        fields[i] = float(fields[i]) if fields[i] != "None" else None
-                    fields[0] = datetime.datetime.fromtimestamp(fields[0], datetime.timezone.utc)
-                    fields.insert(0, ident)
+                    # fields[1] = datetime.datetime.fromtimestamp(fields[1], datetime.timezone.utc)
+                    fields[2] = float(fields[2])
+                    fields[3] = float(fields[3])
                     cur.execute(sql0, fields)
                     cnt += 1
                 except:
@@ -100,20 +92,17 @@ parser = ArgumentParser()
 Logger.addArgs(parser)
 parser.add_argument("--sql", type=str, default="ship.sql",
                     help="Filename with table definitions")
-parser.add_argument("--csv", type=str, default="~/Sync.ARCTERX/Ship/Ship",
+parser.add_argument("--csv", type=str, default="~/Sync.ARCTERX/Ship/pos.csv",
         help="Input directory to monitor for changes")
 parser.add_argument("--db", type=str, default="arcterx", help="Which Postgresql DB to use")
 parser.add_argument("--delay", type=float, default=10,
         help="Number of seconds after an update until loading the changes")
+parser.add_argument("--force", action="store_true", help="Force loading entire CSV file")
 args = parser.parse_args()
 
 Logger.mkLogger(args, fmt="%(asctime)s %(levelname)s: %(message)s")
 
 args.csv = os.path.abspath(os.path.expanduser(args.csv))
-
-if not os.path.isdir(args.csv):
-    logging.error("%s is not a directory", args.csv)
-    sys.exit(1)
 
 with psycopg.connect(f"dbname={args.db}") as db:
     loadAndExecuteSQL(db, args.sql)
@@ -126,7 +115,7 @@ rdr = Reader(args, inotify.queue)
 inotify.start()
 rdr.start()
 
-inotify.addTree(args.csv)
+inotify.addTree(os.path.dirname(args.csv))
 
 try:
     Thread.waitForException()
