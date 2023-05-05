55c3f848b9874b18d0846a2eeb38fa73a0563f09
Pat Welch <pat@mousebrains.com>
2023-05-05 21:42:09 +0000

    Switch to rsync

--- CruiseSync/syncADCPs.sh
+++ CruiseSync/syncADCPs.sh
@@ -4,14 +4,41 @@
 #
 # April-2023, Pat Welch, pat@mousebrains.com
 
-/home/pat/ARCTERX/CruiseSync/syncADCP.py \
-	--output=~/Sync.ARCTERX/Ship/ADCP/wh300 \
-	--ADCP=/mnt/ship/TN417/adcp/proc/wh300/contour \
-	--logfile=/home/pat/logs/syncADCPwh300.log \
-	--verbose
+ADCP=/mnt/ship/TN417/adcp/proc
+WH300=$ADCP/wh300/contour
+OS75=$ADCP/os75nb/contour
+LOG=/home/pat/logs/syncADCPs.log
+CACHE=/home/pat/cache
+TGT=/home/pat/Sync.ARCTERX/Ship/ADCP
 
-/home/pat/ARCTERX/CruiseSync/syncADCP.py \
-	--output=~/Sync.ARCTERX/Ship/ADCP/os75nb \
-	--ADCP=/mnt/ship/TN417/adcp/proc/os75nb/contour \
-	--logfile=/home/pat/logs/syncADCPos75nb.log \
-	--verbose
+echo >>$LOG
+date >> $LOG
+echo >>$LOG
+
+/usr/bin/rsync \
+	--temp-dir=$CACHE \
+	--archive \
+	--verbose \
+	$WH300/allbins_depth.mat \
+	$WH300/allbins_other.mat \
+	$WH300/allbins_u.mat \
+	$WH300/allbins_v.mat \
+	$WH300/allbins_w.mat \
+	$WH300/allbins_amp.mat \
+	$WH300/*.nc \
+	$TGT/wh300 \
+	2>&1 >>$LOG
+
+/usr/bin/rsync \
+	--temp-dir=$CACHE \
+	--archive \
+	--verbose \
+	$OS75/allbins_depth.mat \
+	$OS75/allbins_other.mat \
+	$OS75/allbins_u.mat \
+	$OS75/allbins_v.mat \
+	$OS75/allbins_w.mat \
+	$OS75/allbins_amp.mat \
+	$OS75/*.nc \
+	$TGT/os75nb \
+	2>&1 >>$LOG
