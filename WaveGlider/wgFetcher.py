db451ebdf3f2ab2538d047c18d34587cc5086ad9
Pat Welch <pat@mousebrains.com>
2023-05-11 18:04:44 +0000

    Add in xband

--- WaveGlider/wgFetcher.py
+++ WaveGlider/wgFetcher.py
@@ -54,6 +54,9 @@ class PositionFile:
     def name(self, name:str) -> None:
         self.__name = name
 
+    def done(self) -> None:
+        pass
+
     def commit(self) -> None:
         self.__db.commit();
         self.__curosr = None;
@@ -98,26 +101,105 @@ class PositionFile:
                 ofp.write(msg)
             logging.info("Updated %s", ofn)
 
-class RetrieveFile:
+class MWBfile:
+    def __init__(self, args:ArgumentParser) -> None:
+        self.size = 0
+        self.__args = args
+        self.__db = psycopg.connect(f"dbname={args.db}")
+        self.__cursor = self.__db.cursor()
+        loadAndExecuteSQL(self.__db,
+                          os.path.abspath(os.path.expanduser(args.schema)),
+                          "glider")
+        self.__sql = f"INSERT INTO {args.table}"
+        self.__sql+= "(grp,id,t,lat,lon) VALUES(%s,%s,%s,%s,%s)"
+        self.__sql+= " ON CONFLICT DO NOTHING;"
+        self.__name = None
+        self.__csvDir = os.path.abspath(os.path.expanduser(args.csvSaveTo))
+        if not os.path.isdir(self.__csvDir):
+            logging.info("Creating %s", self.__csvDir)
+            os.makedirs(self.__csvDir, mode=0o755, exist_ok=True)
+
+    def addArgs(parser:ArgumentParser) -> None:
+        grp = parser.add_argument_group(description="MWB related options")
+        grp.add_argument("--mwbSaveTo", type=str, default="~/Sync.ARCTERX/Shore/MWB")
+
+    def __del__(self):
+        if self.__db: 
+            if self.__cursor: self.__db.commit()
+            self.__db.close()
+
+    def name(self, name:str) -> None:
+        self.__name = name
+
+    def commit(self) -> None:
+        self.__db.commit();
+        self.__curosr = None;
+
+    def block(self, data:bytes) -> None:
+        self.size += len(data)
+        args = self.__args
+        regexp = self.__regexp
+        sql = self.__sql
+        cur = self.__cursor
+        values = [args.className, self.__name, None, None, None]
+
+        for line in data.split(b"\n"):
+            matches = regexp.fullmatch(line)
+            if not matches:
+                # logging.debug("Unmatched line: %s", line)
+                continue
+            values[2] = datetime.datetime.strptime(
+                    str(matches[1], "UTF-8"),
+                    "%Y-%m-%d %H:%M:%S").replace(tzinfo=datetime.timezone.utc)
+            values[3] = float(str(matches[2], "UTF-8"))
+            values[4] = float(str(matches[4], "UTF-8"))
+            cur.execute(sql, values, prepare=True)
+
+    def toCSV(self) -> None:
+        args = self.__args
+        csvDir = self.__csvDir
+        tbl = args.table
+        sql = f"UPDATE {tbl} SET qCSV=TRUE WHERE CTID IN ("
+        sql+= f"SELECT DISTINCT ON (grp,id) CTID FROM {tbl} ORDER BY grp,id,t DESC"
+        sql+= ") AND qCSV=FALSE"
+        sql+= " RETURNING grp,id,t,lat,lon;"
+        cur = self.__cursor
+        cur.execute(sql)
+        for row in cur:
+            ofn = os.path.join(csvDir, row[0] + "_" +  row[1] + ".pos.csv")
+            mode = "a" if os.path.isfile(ofn) else "w"
+            with open(ofn, mode) as ofp:
+                if mode == "w":
+                    ofp.write("t,lat,lon\n")
+                msg = str(round(row[2].timestamp())) + f",{row[3]:.6f},{row[4]:.6f}\n"
+                ofp.write(msg)
+            logging.info("Updated %s", ofn)
+
+class CopyFile:
     def __init__(self, fn:str) -> None:
         self.__filename = fn
         self.__fp = None
         self.size = 0
-        self.offset = os.path.size(fn) if os.path.isfile(fn) else 0
 
     @staticmethod
     def addArgs(parser:ArgumentParser) -> None:
-        parser.add_argument("--ftpSaveTo", type=str, default="~/Sync.ARCTERX/Shore/WG")
+        pass
 
     def __del__(self):
         if self.__fp is not None:
             self.__fp.close()
             self.__fp = None
 
+    def done(self) -> None:
+        pass
+
     def block(self, data:bytes) -> None:
-        if self.__fp is None:
-            mode = "a" if self.__offset else "w"
-            self.__fp = open(self.__filename, mode + "b")
+        if not self.__fp:
+            dirname = os.path.dirname(self.__filename)
+            if not os.path.isdir(dirname):
+                logging.info("Creating %s", dirname)
+                os.makedirs(dirname, mode=0o755, exist_ok=True)
+            self.__fp = open(self.__filename, mode="wb")
         self.__fp.write(data)
         self.size += len(data)
 
@@ -128,45 +210,74 @@ class FTPfetch:
             # logging.info("Making %s", args.ftpSaveTo)
             # os.makedirs(args.ftpSaveTo, mode=0o755, exist_ok=True)
 
-        regexp = re.compile(args.ftpRegEx)
-
         (username, password) = getCredentials(
                 os.path.abspath(os.path.expanduser(args.ftpCredentials)))
 
         with FTP(host=args.ftpHost, user=username, passwd=password) as ftp:
             ftp.set_pasv(True) # Turn on passive mode
             objPos = None
-            for directory in args.ftpDirectory:
+            objMWB = None
+            for pattern in args.ftpDirectory:
+                directory = os.path.dirname(pattern)
+                expr = re.compile(os.path.basename(pattern))
                 logging.debug("CWD to %s", directory)
                 ftp.cwd(directory)
                 for fn in ftp.nlst():
-                    matches = regexp.fullmatch(fn)
+                    matches = expr.fullmatch(fn)
                     if not matches:
                         logging.info("Rejecting %s", fn)
                         continue
-                    if matches[3] == "positions_last_24h":
+                    if "positions_last_24h" in fn:
+                        matches = re.fullmatch(r"wg_SV3-(\d+)_(\w+)_positions_last_24h[.](txt|csv)", fn)
+                        if not matches: continue
                         if objPos is None:
                             objPos = PositionFile(args)
-                        objPos.name(matches[2])
                         obj = objPos
-                        sz0 = objPos.size
+                        obj.name(matches[2])
+                        sz0 = obj.size
+                        offset = None
+                    elif "_hfr_pw_6km_rtv_uwls_" in fn:
+                        ofn = os.path.join(args.output, "HFR", fn)
+                        if os.path.isfile(ofn): continue
+                        obj = CopyFile(ofn)
+                        sz0 = 0
+                        offset = None
+                    elif re.fullmatch(r"\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}[.]png", fn):
+                        ofn = os.path.join(args.output, "XBand", fn)
+                        if os.path.isfile(ofn): continue
+                        obj = CopyFile(ofn)
+                        sz0 = 0
+                        offset = None
+                    elif "mwb" in fn:
+                        logging.info("MWB %s", fn);
+                        continue
+                        ofn = os.path.join("data", "MWB", fn)
+                        # if os.path.isfile(ofn): continue
+                        if not objMWB:
+                            objMWB = MWBfile(args)
+                        obj = objMWB
+                        obj.name(fn)
+                        sz0 = obj.size
                         offset = None
                     else:
-                        logging.error("Unsupported file type, %s", fn)
+                        logging.info("Unsupported file %s", fn)
                         continue
-                    # fnOut = os.path.join(args.ftpSaveTo, fn)
-                    # offset = 0
-                    # if os.path.exists(fnOut) and not re.search(r"_last_24h", fnOut):
-                        # info = os.stat(fnOut)
-                        # offset = info.st_size
-                    # obj = RetrieveFile(fnOut, offset)
                     ftp.retrbinary(f"RETR {fn}", obj.block, blocksize=65536, rest=offset)
-                    if sz0 is None: # pos
-                        logging.info("Fetched %s bytes from %s", objPos.size - sz0, fn, offset)
+                    obj.done()
+                    if offset is None:
+                        logging.info("Fetched %s bytes from %s", obj.size - sz0, fn)
+                    else:
+                        logging.info("Fetched %s bytes from %s offset %s", obj.size - sz0, fn, offset)
+
             if objPos is not None:
                 objPos.toCSV() # After all the records are fetched, update the CSV files if needed
                 objPos.commit()
                 objPos = None
+
+            if objMWB is not None:
+                objMWB.toCSV() # After all the records are fetched, update the CSV files if needed
+                objMWB.commit()
+                objMWB = None
 
     @staticmethod
     def addArgs(parser:ArgumentParser) -> None:
@@ -181,27 +292,29 @@ class FTPfetch:
                 help="Fully qualified hostname to connect to")
         grp.add_argument("--ftpCredentials", type=str, default="~/.config/SIO/.sio.credentials",
                 help="Name of JSON file containinng the SIO credentials")
-        grp.add_argument("--ftpRegEx", type=str,
-                default=r"wg_SV3-(\d+)_(\w+)_(positions_last_24h)[.](txt|csv)$",
-                help="Regular expression files must match to be fetched")
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     Logger.addArgs(parser)
     FTPfetch.addArgs(parser)
     PositionFile.addArgs(parser)
-    RetrieveFile.addArgs(parser)
+    CopyFile.addArgs(parser)
+    MWBfile.addArgs(parser)
+    # RetrieveFile.addArgs(parser)
     grp = parser.add_argument_group(description="Output related options")
-    grp.add_argument("--output", type=str, default="data", help="Output directory")
+    grp.add_argument("--output", type=str, default="~/Sync.ARCTERX/Shore", help="Output directory")
     args = parser.parse_args()
 
     Logger.mkLogger(args, fmt="%(asctime)s %(levelname)s: %(message)s", logLevel="INFO")
 
+    args.output = os.path.abspath(os.path.expanduser(args.output))
+
     if args.ftpDirectory is None:
         args.ftpDirectory = (
-                "CORDC/outgoing/arcterx",
-                # "CORDC/outgoing/arcterx/wgms",
-                # "CORDC/outgoing/arcterx/sensor_data",
+                "/CORDC/outgoing/arcterx/wg_SV3-.*_positions_last_24h[.]txt",
+                "/CORDC/outgoing/arcterx/hfr/NetCDF/2.*[.]nc",
+                "/CORDC/outgoing/arcterx/xband/2.*[.]png",
+                "/CORDC/outgoing/internal/arcterx/wavebuoy/mwb.*[.]nc",
                 )
 
     FTPfetch(args)
