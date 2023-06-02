52583a10fb359275081c564d5edf764f43c3d87f
Pat Welch <pat@mousebrains.com>
2023-06-02 06:38:21 +0000

    Add suport for alto floats

--- Floats/zoli2DB.py
+++ Floats/zoli2DB.py
@@ -34,7 +34,7 @@ class Reader(Thread):
 
         cnt = 0
         with psycopg.connect(dbOpt) as db: 
-            for fn in glob.glob(os.path.join(args.zoli, "gps_log.txt")):
+            for fn in glob.glob(os.path.join(args.zoli, "*gps_log.txt")):
                 if "share" in fn: continue
                 cnt += self.__loadFile(db, fn, args.group)
             if cnt:
@@ -56,18 +56,23 @@ class Reader(Thread):
         sql0+= " VALUES(%s,%s,%s,%s,%s)"
         sql0+= " ON CONFLICT DO NOTHING;"
 
-        sql1 = "SELECT position FROM filePosition WHERE filename=%s;"
+        # sql1 = "SELECT position FROM filePosition WHERE filename=%s;"
 
-        sql2 = "INSERT INTO filePosition VALUES (%s,%s)"
-        sql2+= " ON CONFLICT (filename) DO UPDATE SET position=excluded.position;"
+        # sql2 = "INSERT INTO filePosition VALUES (%s,%s)"
+        # sql2+= " ON CONFLICT (filename) DO UPDATE SET position=excluded.position;"
 
         cur = db.cursor()
         pos = None
-        for row in cur.execute(sql1, [fn]):
-            pos = row[0]
-            break
+        # for row in cur.execute(sql1, [fn]):
+            # pos = row[0]
+            # break
 
         ident = os.path.basename(os.path.dirname(fn))
+        qAlto = "alto" in fn
+        if qAlto:
+            tFormat = "%Y-%m-%dT%H:%M:%SZ"
+        else:
+            tFormat = "%m/%d/%Y %H:%M:%S"
         cur.execute("BEGIN TRANSACTION;")
         cnt = 0;
         with open(fn, "r") as fp:
@@ -75,14 +80,15 @@ class Reader(Thread):
                 fp.seek(pos)
             for line in fp:
                 fields = line.strip().split(",")
-                if len(fields) < 4: continue # Truncated line so ignore
+                if len(fields) < (4+qAlto): continue # Truncated line, so ignore
+                if fields[0] == "flnum": continue # Alto header
                 try:
                     args = (
                             grp,
                             ident,
-                            datetime.datetime.strptime(fields[1], "%m/%d/%Y %H:%M:%S"),
-                            float(fields[2]),
-                            float(fields[3]),
+                            datetime.datetime.strptime(fields[1+qAlto], tFormat),
+                            float(fields[2+qAlto]),
+                            float(fields[3+qAlto]),
                             )
                     cur.execute(sql0, args)
                     cnt += 1
@@ -92,7 +98,7 @@ class Reader(Thread):
             if cnt:
                 ipos = pos
                 pos = fp.tell()
-                cur.execute(sql2, (fn, pos))
+                # cur.execute(sql2, (fn, pos))
                 logging.info("Loaded %s cnt %s pos %s -> %s", fn, cnt, ipos, pos)
                 db.commit();
             else:
@@ -145,7 +151,7 @@ Logger.addArgs(parser)
 parser.add_argument("--group", type=str, default="FL", help="Group name being saved to")
 parser.add_argument("--csv", type=str, default="~/Sync.ARCTERX/Shore/Floats/zoli.csv",
         help="Output directory")
-parser.add_argument("--zoli", type=str, default="~zszuts/floatdata/emapextwr/*arc1",
+parser.add_argument("--zoli", type=str, default="~zszuts/floatdata/*/*arc1",
         help="Input directory to monitor for changes")
 parser.add_argument("--db", type=str, default="arcterx", help="Which Postgresql DB to use")
 parser.add_argument("--delay", type=float, default=10,
