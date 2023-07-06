2f4b761bfdac5a3679a8af872e7377612587ba2b
Pat Welch <pat@mousebrains.com>
2023-07-06 04:24:39 +0000

    Pull out updateCSV from UW_SG.py

--- Gliders/UW_SG.py
+++ Gliders/UW_SG.py
@@ -16,45 +16,6 @@ import psycopg
 import os
 import requests
 
-def updateCSV(db, grp:str, fn:str, qForce:bool) -> None:
-    sql0 = "CREATE TEMPORARY TABLE tpwUW (LIKE glider);"
-
-    sql1 = "WITH updated AS ("
-    sql1+= "UPDATE glider SET qCSV=TRUE"
-    sql1+= " WHERE grp=%s"
-    if not args.force: sql1+= " AND NOT qCSV"
-    sql1+= " RETURNING grp,id,t,lat,lon"
-    sql1+= ")"
-    sql1+= "INSERT INTO tpwUW SELECT * FROM updated;"
-
-    sql2 = "SELECT id,t,lat,lon"
-    sql2+= " FROM tpwUW ORDER BY t;"
-
-    cur = db.cursor()
-    cur.execute(sql0)
-    cur.execute(sql1, (grp,))
-
-    nCreated = 0
-    nOpened = 0
-    cnt = 0
-    fp = None
-    for row in cur.execute(sql2):
-        if not fp:
-            if args.force or not os.path.exists(fn):
-                logging.info("Creating %s", fn)
-                nCreated += 1
-                fp = open(fn, "w")
-                fp.write("id,t,lat,lon\n")
-            else:
-                logging.info("Opening %s", fn)
-                nOpened += 1
-                fp = open(fn, "a")
-        fp.write(",".join(map(str, row)) + "\n")
-        cnt += 1
-
-    if fp: fp.close()
-    logging.info("Fetched %s CSV records created %s opened %s", cnt, nCreated, nOpened)
-
 def loadURL(url:str) -> dict:
     with requests.get(url) as r:
         if not r.ok:
@@ -75,11 +36,7 @@ parser = ArgumentParser()
 Logger.addArgs(parser)
 parser.add_argument("--grp", type=str, default="AUV", help="Group name")
 parser.add_argument("--url", type=str, action="append", help="URL(s) to fetch")
-parser.add_argument("--csv", type=str, default="~/Sync.ARCTERX/Shore/Gliders/UW.csv",
-        help="Where to CSV filename")
 parser.add_argument("--db", type=str, default="arcterx", help="Which Postgresql DB to use")
-parser.add_argument("--force", action="store_true", help="Rebuild the CSV file from scratch")
-grp = parser.add_mutually_exclusive_group()
 args = parser.parse_args()
 
 Logger.mkLogger(args, fmt="%(asctime)s %(levelname)s: %(message)s")
@@ -106,4 +63,3 @@ with psycopg.connect(f"dbname={args.db}") as db: # Get the last time stored in t
         cur = db.cursor()
         saveToDB(cur, args.grp, ident, info)
         db.commit()
-    updateCSV(db, args.grp, args.csv, args.force)
---
+++ Gliders/updateCSV.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python3
+#
+# Extract rows from database where qCSV is true
+# and save into a CSV file.
+#
+# Ripped out of UW_SG.py
+#
+# July-2023, Pat Welch, pat@mousebrains.com
+
+from argparse import ArgumentParser
+from TPWUtils import Logger
+import logging
+import psycopg
+import os
+
+parser = ArgumentParser()
+Logger.addArgs(parser)
+parser.add_argument("--grp", type=str, default="AUV", help="Group name")
+parser.add_argument("--csv", type=str, default="~/Sync.ARCTERX/Shore/Gliders/UW.csv",
+                    help="Where to CSV filename")
+parser.add_argument("--db", type=str, default="arcterx", help="Which Postgresql DB to use")
+parser.add_argument("--force", action="store_true", help="Rebuild the CSV file from scratch")
+args = parser.parse_args()
+
+Logger.mkLogger(args, fmt="%(asctime)s %(levelname)s: %(message)s")
+
+args.csv = os.path.abspath(os.path.expanduser(args.csv))
+
+sql0 = "CREATE TEMPORARY TABLE TPW (LIKE glider);"
+
+sql1 = "WITH updated AS ("
+sql1+= "UPDATE glider SET qCSV=TRUE"
+sql1+= " WHERE grp=%s"
+if not args.force: sql1+= " AND NOT qCSV"
+sql1+= " RETURNING grp,id,t,lat,lon"
+sql1+= ")"
+sql1+= "INSERT INTO TPW SELECT * FROM updated;"
+
+sql2 = "SELECT id,t,lat,lon FROM TPW ORDER BY t;"
+
+if args.force or not os.path.exists(args.csv):
+    dirname = os.path.dirname(args.csv)
+    if not os.path.isdir(dirname):
+        logging.info("Creating %s", dirname)
+        os.makedirs(dirname, mode=0o755, exist_ok=True)
+
+    logging.info("Creating %s", args.csv)
+    fp = open(args.csv, "w")
+    fp.write("id,t,lat,lon\n")
+else:
+    logging.info("Opening %s", args.csv)
+    fp = open(args.csv, "a")
+
+with psycopg.connect(f"dbname={args.db}") as db: # Get the last time stored in the database
+    try:
+        cur = db.cursor()
+        cur.execute(sql0)
+        cur.execute("BEGIN TRANSACTION;")
+        cur.execute(sql1, (args.grp,))
+
+        cnt = 0
+        for row in cur.execute(sql2):
+            fp.write(",".join(map(str, row)) + "\n")
+            cnt += 1
+
+        if fp: fp.close()
+        db.commit()
+        logging.info("Fetched %s CSV records into %s", cnt, args.csv)
+    except:
+        db.rollback()
+        logging.exception("Unexpected error %s", args)
