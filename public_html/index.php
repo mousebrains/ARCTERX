6baf86de22f4c371caac67fdd5cec95ae728383c
Pat Welch <pat@mousebrains.com>
2025-01-11 20:38:55 +0000

    SI changes

--- public_html/index.php
+++ public_html/index.php
@@ -11,30 +11,22 @@ if (is_dir("AVISO")) {
 	echo "<li><a href='AVISO'>AVISO</a></li>\n";
 }
 ?>
-<li><a href='links.html'>Links</a></li>
-<li><a href='Sync.ARCTERX/Shore/Papers'>Papers</a></li>
-<li><a href='Sync.ARCTERX/Shore/Software'>Software</a></li>
-<li><a href='Sync.ARCTERX/Ship'>Ship</a></li>
-<li><a href='Sync.ARCTERX/Shore'>Shore</a></li>
-<li><a href='typhoons.html'>Typhoons</a></li>
-<li><a href='https://www.pacioos.hawaii.edu/voyager'>PacIOOS model</a></li>
-<li><a href='https://vertmix.alaska.edu/atx2023.html'>APL-UW Epsilon PALAU 800 model</a></li>
-<li><a href='https://www7320.nrlssc.navy.mil/NLIWI_WWW/EAS-SKII/EASNFS.html'>NRL model</a></li>
+<li><a href='Sync/Shore/Papers'>Papers</a></li>
+<li><a href='Sync/Shore/Software'>Software</a></li>
+<li><a href='Sync/Ship'>Ship</a></li>
+<li><a href='Sync/Shore'>Shore</a></li>
+<li><a href='Sync/Processed'>Processed</a></li>
 </ul>
 <pre>
 <?php
-if (gethostname() != "arcterx") {
+if (gethostname() != "arcterx.ceoas.oregonstate.edu") {
 	echo "<div>";
 	echo "<hr>";
-	echo "<h3>Mounting shore to ship and ship to shore data</h3>";
+	echo "<h3>SMB mount points</h3>";
 	echo "Any of:";
 	echo "<ul>";
-	echo "<li>smb://arc2.local/ARCTERX</li>";
-	echo "<li>smb://arc3.local/ARCTERX</li>";
-	echo "<li>smb://arc1.local/ARCTERX</li>";
-	echo "<li>smb://10.43.25.116/ARCTERX</li>";
-	echo "<li>smb://10.43.25.115/ARCTERX</li>";
-	echo "<li>smb://10.43.25.114/ARCTERX</li>";
+	echo "<li>smb://pi10</li>";
+	echo "<li>smb://shiphouse</li>";
 	echo "</ul>";
 	echo "</div>";
 }
